b85b70a443f8107de933d265fb312a85e7760bd2
Mosihere <Mostafakhoshhal@yahoo.com>
2024-05-06 01:53:28 +0330

    search Movies by name using search query parameter/ search movie links by movie_id implemeted as well.

--- movies/views.py
+++ movies/views.py
@@ -11,14 +11,34 @@ class MovieList(ReadOnlyModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+    def get_queryset(self):
+        queryset = Movie.objects.all()
+        movie_name = self.request.query_params.get('search')
+        if movie_name:
+            new_queryset = queryset.filter(name__iexact=movie_name)
+
+            if new_queryset.exists():
+                return new_queryset
+        
+            else:
+                queryset = queryset.filter(name__icontains=movie_name)
+                return queryset
+
+        return queryset
+
 
 class LinkList(ReadOnlyModelViewSet):
     serializer_class = LinkSerializer
 
     def get_queryset(self):
         queryset = Link.objects.all()
+        movie_id = self.request.query_params.get('movie_id')
         movie_name = self.request.query_params.get('movie_name')
-        if movie_name:
+
+        if movie_id:
+            queryset = queryset.filter(movie__id=movie_id)
+
+        elif movie_name:
             new_queryset = queryset.filter(movie__name__iexact=movie_name)
 
             if new_queryset.exists():
