90265cb4a013c9ce9608968a2a45eaed952c1e2f
Mosihere <Mostafakhoshhal@yahoo.com>
2024-05-06 20:44:06 +0330

    remove unused imports/ enhace queries/ eagerload foreignkey relationship for movies for Link Entity

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
 from .models import Movie, Link
-from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .serializers import MovieSerializer, LinkSerializer
 
@@ -14,16 +12,11 @@ class MovieList(ReadOnlyModelViewSet):
     def get_queryset(self):
         queryset = Movie.objects.all()
         movie_name = self.request.query_params.get('search')
+
         if movie_name:
-            new_queryset = queryset.filter(name__iexact=movie_name)
-
-            if new_queryset.exists():
-                return new_queryset
+            queryset = queryset.filter(name__icontains=movie_name)
+            return queryset[:10]
         
-            else:
-                queryset = queryset.filter(name__icontains=movie_name)
-                return queryset
-
         return queryset
 
 
@@ -31,21 +24,17 @@ class LinkList(ReadOnlyModelViewSet):
     serializer_class = LinkSerializer
 
     def get_queryset(self):
-        queryset = Link.objects.all()
+        queryset = Link.objects.select_related('movie').all()
         movie_id = self.request.query_params.get('movie_id')
         movie_name = self.request.query_params.get('movie_name')
 
         if movie_id:
             queryset = queryset.filter(movie__id=movie_id)
-
-        elif movie_name:
-            new_queryset = queryset.filter(movie__name__iexact=movie_name)
-
-            if new_queryset.exists():
-                return new_queryset
+            return queryset[:15]
         
-            else:
-                queryset = queryset.filter(movie__name__icontains=movie_name)
-                return queryset
+        elif movie_name:
+            queryset = queryset.filter(movie__name__icontains=movie_name)
+            return queryset[:15]
 
-        return queryset
+        else:
+            return queryset
