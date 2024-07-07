589baf9410f2b674fa4748ba54a63ebb4c55bf64
Mosihere <Mostafakhoshhal@yahoo.com>
2024-07-07 18:37:53 +0330

    adding load balance test for Movie endopoint aswell, with search query_param

--- locustfiles/browse_movies.py
+++ locustfiles/browse_movies.py
@@ -1,15 +1,17 @@
 from locust import HttpUser, task, between
-from random import randint
+from random import randint, choice
+from string import ascii_lowercase
 
 
 class WebsiteUser(HttpUser):
     wait_time = between(1, 5)
-    @task(2)
+    @task(4)
     def view_links(self):
-        movie_id = randint(1, 104)
+        movie_id = randint(1, 5200)
         self.client.get(f'/api/links/?movie_id={movie_id}', name='/api/links')
 
     @task(4)
-    def view_link(self):
-        link_id = randint(187, 688)
-        self.client.get(f'/api/links/{link_id}/', name='/api/links/:id/')
+    def view_movies(self):
+        movie_name = choice(ascii_lowercase)
+        print(movie_name)
+        self.client.get(f'/api/movies/?search={movie_name}', name='/api/movies')
