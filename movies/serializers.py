deda15b6c111f8e0e3ce8954f4bbf131ef3861b4
Mosihere <Mostafakhoshhal@yahoo.com>
2024-09-15 22:43:06 +0330

    change name of related-fields of LinkSerializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -15,5 +15,5 @@ class LinkSerializer(serializers.Serializer):
     link = serializers.CharField(max_length=255)
     quality = serializers.CharField(max_length=10)
     codec = serializers.CharField(max_length=4)
-    movie_name = serializers.CharField(source='movie__name')
-    movie_published_at = serializers.IntegerField(source='movie__published_at')
+    name = serializers.CharField(source='movie__name')
+    published_at = serializers.IntegerField(source='movie__published_at')
